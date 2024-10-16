ffd733cb9fc0188244905f425a08795e2e81ed13
danielta18 <153893558+danielta18@users.noreply.github.com>
2024-10-16 18:40:21 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".gov", ".org"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < extension.length; e++)
+          document.querySelector(
+            "#p-tag"
+          ).innerHTML += `<h3>${pronoun[p]}${adj[a]}${noun[n]}${extension[e]}</h3>`;
+      }
+    }
+  }
 };
